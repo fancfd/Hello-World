30aa23b320d0e35fb20236287e1d0020d8f92ecf
Fan Chun <fanchun@pku.edu.cn>
2019-08-12 09:30:53 +0800

    Create xtimer.cpp

---
+++ xtimer.cpp
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //  Start Timers
+    double wall0 = get_wall_time();
+    double cpu0  = get_cpu_time();
+
+    //  Perform some computation.
+    double sum = 0;
+#pragma omp parallel for reduction(+ : sum)
+    for (long long i = 1; i < 10000000000; i++){
+        sum += log((double)i);
+    }
+
+    //  Stop timers
+    double wall1 = get_wall_time();
+    double cpu1  = get_cpu_time();
+
+    cout << "Wall Time = " << wall1 - wall0 << endl;
+    cout << "CPU Time  = " << cpu1  - cpu0  << endl;
+
+    //  Prevent Code Elimination
+    cout << endl;
+    cout << "Sum = " << sum << endl;
+
+}
